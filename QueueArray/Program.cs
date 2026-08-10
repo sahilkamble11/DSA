75f5485c5fc893a05342beb0213c519352f8926f
Sahil Kamble <sahilbajkamble@gmail.com>
2026-08-10 13:20:51 +0530

    Implemented Queue using array

---
+++ QueueArray/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace QueueArray;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Queue queue= new Queue(3);
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+       // queue.Enqueue(4);
+        //queue.Peek();
+        queue.Display();
+
+        queue.Dequeue();
+        queue.Display();
+    }
+}
---
+++ QueueArray/Queue.cs
@@ -0,0 +1,70 @@
+namespace QueueArray;
+
+public class Queue
+{
+    int size;
+    int front;
+    int rear;
+    int[] arr;
+    public Queue(int size)
+    {
+        this.size=size;
+        arr=new int[size];
+        front=0;
+        rear=-1;
+    }
+
+    public void Enqueue(int data)
+    {
+        if (rear == size - 1)
+        {
+            Console.WriteLine("Queue is full..");
+            return;
+        }
+
+        rear++;
+        arr[rear]=data;
+
+    }
+
+    public void Dequeue()
+    {
+        if (front > rear)
+        {
+            Console.WriteLine("Queue is Empty..");
+            return;
+        }
+        Console.WriteLine();
+        Console.WriteLine("Removed: "+ arr[front]);
+        
+        front++;
+    }
+
+    public bool IsEmpty()
+    {
+        if (front > rear)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Peek()
+    {
+        if (front > rear)
+        {
+            Console.WriteLine("Queue is Empty..");
+            return;
+        }
+        Console.WriteLine("First Element: "+arr[front]); 
+    }
+
+    public void Display()
+    {
+        for(int i = front ; i <= rear; i++)
+        {
+            Console.Write(arr[i]+"<--");
+        }
+    }
+
+}
