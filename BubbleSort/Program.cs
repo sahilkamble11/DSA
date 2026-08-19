6735fc5747231beed89200300b4c157e1056f42a
Sahil Kamble <sahilbajkamble@gmail.com>
2026-08-19 15:09:57 +0530

    Bubble sort implemented

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int [] arr={6,3,5,4,1,2};
+
+for(int i = 0 ; i < arr.Length - 1; i++)
+{
+    for(int j = 0 ; j < arr.Length - 1 - i ; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp; 
+        } 
+    }
+}
+
+ foreach(int x in arr)
+        {
+            Console.Write("  "+x);
+        }
+
+
+
+    
+
+
+
